8e43bfb25c9840f9244eba64f6b194b1a6d20c84
mjones12209 <m.jones12209@gmail.com>
2021-02-20 15:01:00 -0500

    removed systemd timer/modified install/uninstall of systemd service unit

--- systemd/install.sh
+++ systemd/install.sh
@@ -1,3 +1,3 @@
-cp scrapyd.service scrapyd.timer /usr/lib/systemd/system
+cp scrapyd.service /usr/lib/systemd/system
 systemctl daemon-reload
-systemctl enable --now scrapyd.timer
+systemctl enable --now scrapyd.serviceq
